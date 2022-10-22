ebf330db3393c680b66108c1674c469795f7103a
Kirill Kornev <kirill_kornew@mail.ru>
2022-10-22 18:05:09 +0600

    Added navigation

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Kirill Kornev on 22.10.2022.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+    }
+}
