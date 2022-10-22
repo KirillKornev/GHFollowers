17c8f5f22b59edcc98925b00e1b76ee49833f66b
Kirill Kornev <kirill_kornew@mail.ru>
2022-10-22 18:22:20 +0600

    Added custom elements

---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,42 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Kirill Kornev on 22.10.2022.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Private
+
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+
+        placeholder = "Enter a username"
+    }
+}
