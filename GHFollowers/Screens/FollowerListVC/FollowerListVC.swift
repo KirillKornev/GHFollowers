8029bad714426afad8f1619ee77de3f4084ebfcf
Kirill Kornev <56608476+KirillKornev@users.noreply.github.com>
2022-11-23 00:42:40 +0400

    Tech/empty state (#8)
    * Refactoring
    
    * Added empty state
    
    * Changed constraints
    
    * Realised search mechanic

--- GHFollowers/Screens/FollowerListVC/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC/FollowerListVC.swift
@@ -11,6 +11,7 @@ class FollowerListVC: UIViewController {
 
     var userName: String? = ""
     private(set) var followers: [Follower] = []
+    private var filteredFolloers: [Follower] = []
     private var page = 1
     private var hasMoreFollowers = true
 
@@ -44,6 +45,7 @@ class FollowerListVC: UIViewController {
 
         setupUI()
         setupCollection()
+        configureSearchController()
         fetchFollowers(page: page)
     }
 
@@ -67,29 +69,43 @@ class FollowerListVC: UIViewController {
     private func fetchFollowers(page: Int) {
         showLoadingView()
         NetworkManager.shared.getFollowers(for: userName ?? "", page: page) { [weak self] result in
+            guard let self = self else { return }
             DispatchQueue.main.async {
-                self?.dismissLoadingView()
+                self.dismissLoadingView()
 
                 switch result {
                 case .success(let followers):
-                    if followers.count < 100 { self?.hasMoreFollowers = false }
+                    if followers.count < 100 { self.hasMoreFollowers = false }
+                    self.followers.append(contentsOf: followers)
 
-                    self?.followers.append(contentsOf: followers)
-                    self?.updateData()
+                    if self.followers.isEmpty {
+                        let message = "This user doesn't have any followers. Go follow them 😄"
+                        self.showEmptyStateView(with: message, in: self.view)
+                        return
+                    }
+                    self.updateData(on: self.followers)
                 case .failure(let error):
-                    self?.presentAlert(title: "Bad stuff happened", message: error.rawValue , buttonTitle: "Ok")
+                    self.presentAlert(title: "Bad stuff happened", message: error.rawValue , buttonTitle: "Ok")
                 }
             }
         }
     }
 
-    private func updateData() {
+    private func updateData(on followers: [Follower]) {
         let items: [Item] = followers.map { .follower($0) }
 
         var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
         snapshot.appendSections([.main])
         snapshot.appendItems(items)
         dataSource.apply(snapshot, animatingDifferences: true)
+    }
+
+    private func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search for user name"
+        navigationItem.searchController = searchController
     }
 }
 
@@ -106,5 +122,26 @@ extension FollowerListVC: UICollectionViewDelegate {
             page += 1
             fetchFollowers(page: page)
         }
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+
+extension FollowerListVC: UISearchResultsUpdating {
+
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+
+        filteredFolloers = followers.filter({ $0.login.lowercased().contains(filter.lowercased()) })
+        updateData(on: filteredFolloers)
+    }
+}
+
+// MARK: - UISearchControllerDelegate
+
+extension FollowerListVC: UISearchBarDelegate {
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
     }
 }
